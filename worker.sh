46526c125b3a9f0df337a456033f91537a848dd0
MrSpiderGit <spidermang123@gmail.com>
2017-03-09 11:12:50 +0200

    FInal

--- worker.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-import os
-os.system('sudo apt-get update cache')
-os.system('sudo apt-get install -y git')
-os.system('sudo cp /home/vagrant/hosts/hosts /etc/hosts')
-os.system('sudo mkdir /home/vagrant/.ssh')
-os.system('sudo cp /home/vagrant/hosts/id_rsa.pub /home/vagrant/.ssh/authorized_keys')
-os.system('sudo service ssh restart')
-
