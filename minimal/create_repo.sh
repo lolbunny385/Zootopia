476fdda0f07fb7090ae905cfa99c9c921b9ba609
The-Cooon <thecoon314@gmail.com>
2017-05-14 18:04:09 +0300

    Preparation for benchmark

---
+++ minimal/create_repo.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cp -R /Halbanot/html /var/www
+/var/www/html/debs/amd64/create-repo.sh trusty
--- minimal/gw.sh
+++ minimal/gw.sh
@@ -1,22 +1,13 @@
-#!/bin/bash
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install -y ansible
 sudo apt-get install -y openssh-server
 sudo apt-get install sshpass
-sudo cp /home/vagrant/minimal/hosts /etc/hosts
-ssh-keygen -t rsa -q -f /home/vagrant/.ssh/id_rsa -P ""
-sudo ssh-keygen -t rsa -q -f /home/vagrant/.ssh/id_rsa -P ""
-sudo cp /home/vagrant/minimal/ansible_hosts /etc/ansible/hosts
+sudo cp /Halbanot/git/minimal/hosts /etc/hosts
+ssh-keygen -t rsa -q -f /home/"$0"/.ssh/id_rsa -P ""
+sudo ssh-keygen -t rsa -q -f /home/"$0"/.ssh/id_rsa -P ""
+sudo cp /Halbanot/git/minimal/ansible_hosts /etc/ansible/hosts
+sudo chmod 766 /home/"$0"/.ssh
+sudo chmod 766 /home/"$0"/.ssh/id_rsa
 sudo service ssh restart
-sudo chmod 766 /home/vagrant/.ssh
-sudo chmod 766 /home/vagrant/.ssh/id_rsa
-#files=`cat /home/vagrant/minimal/servers`
-#cho $files
-#for l in $files 
-#do
-#  sudo ssh-keyscan -H $l >> /home/vagrant/.ssh/known_hosts
-#done
-#sudo chown vagrant:vagrant /home/vagrant/.ssh/known_hosts
-sudo service ssh restart
-sudo /home/vagrant/minimal/scan.py
+sudo /home/vagrant/minimal/scan.py "$0"
